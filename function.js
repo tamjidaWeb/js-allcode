da6257c268bfb575edbe2ca7079b4ca784b93cee
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-08-09 18:34:27 +0600

    commited

---
+++ function.js
@@ -0,0 +1,43 @@
+// function sleep(names,time){
+//     console.log(names+ " is sleeping from "+ time);
+// }
+// // console.log(names)
+// sleep('jasim','10pm');
+// sleep('rahim','2pm');
+// sleep('karim','4am');
+// sleep('sadia','9am');
+
+
+
+// function myFunction(a, b) {
+//     // return a * b;
+   
+// }
+// let x = myFunction(4, 3);
+// console.log(x);
+// var carName='bmw';
+// function myFunction() {
+//     let carName = "Volvo";
+//     console.log(carName);
+   
+// }  
+// console.log(carName);
+// myFunction();
+let car = {
+    Name: "John",
+    model: "Doe",
+    weight: '50',
+    Color: "blue",
+    start: function (){
+        console.log('car has started');
+    },
+    drive: function(){
+        console.log('car is driving');
+    }
+  };
+
+  console.log(car.weight);
+//   console.log(car[model]);
+
+car.start();
+
---
+++ index.js
@@ -0,0 +1,1014 @@
+// var isGraduated =true;
+// var salary =75000;
+// var cars=0;
+// // if(isGraduated==true && salary >50000 || cars >=1){
+// //     console.log('eso biye kore feli');
+// // }
+// // else{
+// //     console.log('tor kopale biya nai');
+// // }
+
+// if(isGraduated ==true && salary>50000 || cars){
+//     console.log('cholo biye kori');
+// }
+
+// var money = 44;
+// var danishprice = 45;
+// var butterBread =35;
+// var toastBiscuit =20;
+// if(danishprice <money){
+//     console.log('danish dao mama');
+// }
+// else if(butterBread<money){
+//     console.log('mama butterban dao');
+// }
+// else if (toastBiscuit<money){
+//     console.log('tahole toast e dao mama');
+// }
+// else{
+//     console.log('dhur mama,tk nai,ek cup cha dao to');
+// }
+
+// var destinations = ['sajek','coxsbazar','nepal','bali','sp']
+// // console.log(destinations[2]);
+// destinations[2]='thailand';
+// // console.log(destinations[2]);
+
+// destinations.push('bhutan');
+// destinations.pop();
+// if(destinations[1]=='sajek'){
+//     console.log('sotti');
+// }
+// else if (destinations[2{
+//     console.log('mittha');
+// }
+
+
+var fruits=['apple','banana','orange']
+// var index = fruits.indexOf('banana');
+//  console.log(index);
+//  var ifruits.pop ='orange';
+// fruits.pop('orange');
+// fruits.push('watermelon');
+//  console.log(fruits);
+// var number =70;
+
+
+// if(number>80 ){
+//     console.log('you and jane got a');
+// }
+// else if(number>=60){
+//     console.log('got B')
+// }
+// else{
+//     console.log('got F!!!');
+// }
+
+
+// document.write('find students grade <br>');
+// let marks =prompt('enter scores');
+// if(marks >=80 && marks==85){
+//     document.write('you got a');
+// }
+// else if(marks>=80 && marks==95){
+//     document.write('jane got a');
+// }
+// else if(marks>=80 ){
+//     document.write('jane and i got a');
+// }
+// else if(marks>=60 && marks<80){
+//     document.write('toms got b');
+// }
+// else if(marks>=50 && marks<60){
+//     document.write('peter got c');
+// }
+// else if(marks>=40 && marks<50){
+//     document.write('john got d');
+// }
+// else{
+//     document.write('fail');
+// }
+
+// var num1 =13;
+// var num2 = 79;
+// var num3 = 45;
+// if(num1>num2 && num1>num3){
+//     console.log(num1 + "is big");
+// }
+// else if(num2>num1 && num2>num3){
+//     console.log(num2 + " is big");
+// }
+// else {
+//     console.log(num3 + "is greater");
+// }
+// var num1 =9;
+// var num2 = 8;
+// var num3 = 9;
+// if(num1==num2){
+//     console.log(num1 +" and "+ num2+" isosceles");
+// }
+// else if(num1==num3){
+//     console.log(num1 +" and "+ num3+" isosceles");
+// }
+// else {
+//     console.log(num2 +" and "+ num3+" isosceles");
+// }
+
+
+
+
+
+// for(var i=0; i<=10;i++){
+//     console.log(i);
+// }
+// for(var i=1;i<=10;i+=2){
+//     console.log(i);
+// }
+
+// for(var i=0;i<=10;i++){
+//     console.log(i);
+// }
+// display every element of an array
+// var numbers=[23,76,89,34,76,125,76,45,90,54,99,81];
+// for(var i=0;i<numbers.length;i++){
+//     var number=numbers[i];
+//     if(number>90){
+//         continue;
+//     }
+//     console.log(number);
+   
+// }
+
+// var num=5;
+// while(num>0){
+    
+//     console.log(num);
+//     num--;
+// }
+
+// for(var i=10;i>0;i--){
+//     console.log(i);
+// }
+
+// var items=['bottol','mouse','sunglass','pen'];
+// for(var i=0;i<items.length;i++){
+//     var item=items[i];
+//     console.log(item);
+//     if(item=='sunglass'){
+//         break;
+//     }
+// }
+
+
+// for(var i=0;i<20;i++){
+//     console.log(i);
+//     if(i>5){
+//         break;
+//     }
+// }
+
+// var roastGiven =0;
+// while(roastGiven<10){
+//     console.log('roast den');
+//     roastGiven++;
+//     if(roastGiven>4){
+//         break;
+//     }
+// }
+// var momGiven =1000;
+// var appleprice=400;
+// var orangePrice=300;
+// var expense=appleprice+orangePrice;
+// var returned = momGiven-expense;
+// console.log(returned);
+
+// var math=75.25;
+// var bio=65;
+// var chem=80;
+// var phy=35.45;
+// var bangla=99.50;
+// var avg= (math+bio+chem+phy+bangla)/5;
+// var avgTwoDeci=avg.toFixed(2);
+// console.log(avgTwoDeci);
+
+// var varOne="i am going to be";
+// var varTwo=" an awesome web developer"
+// console.log(varOne + varTwo);
+// console.log("i am going to be" + " an awesome web developer");
+
+
+// var numberGiven=119;
+// var numgiven =5;
+// var remainder =(numberGiven % numgiven);
+// console.log(remainder);
+
+
+// var fruits = ["apple","Banana","Orange"];
+// var IndexFruits=fruits.indexOf("Banana");
+// fruits[IndexFruits]="mango";
+// fruits.pop();
+// fruits.push("watermelon");
+// console.log(fruits);
+// document.write("Students marks <br>");
+// let marks=prompt("please enter numbers");
+
+
+// if(marks>=80){
+//     document.write("got A grade")
+// }
+// else if(marks>=60){
+//     document.write("got B grade");
+// }
+// else if(marks>=50){
+//     document.write(" got C");
+// }
+// else{
+//     document.write(" fail");
+// }
+
+// let marks=40;
+// if(marks>=80){
+//     console.log("got A grade")
+// }
+// else if(marks>=60){
+//     console.log("got B grade");
+// }
+// else if(marks>=50){
+//     console.log(" got C");
+// }
+// else{
+//     console.log(" fail");
+// }
+
+
+// var myname ='khan';
+// var friend=['babul','kabul'
+// ,'kabul','mabul'];
+
+// var thirdfriend=friend[2];
+// friend[3]='dambool';
+
+// if(friend.length<4){
+//     console.log('fokir tk nai');
+// }
+
+// var number =0;
+// while(number<=6){
+//     console.log(number);
+//     number++;
+// }
+// for(var i =0;i<10;i++){
+//     console.log("anything");
+// }
+
+// //function
+// function isMoonUp(time){
+//     if(time >7){
+//         return true;
+//     }
+// }
+// //object
+// var jantus ={
+//     height:6.1,
+//     romantism:'heavy',
+//     nijerTaka:'alhamdulillah',
+// }
+
+// let age =11;
+// age =12;
+// const countryname='bangladesh';
+
+// console.log(age, countryname);
+
+
+//variable
+// var numbers=119;
+// console.log(5%2);
+// console.log(113%2);
+// console.log(171%2);
+// console.log(185%2);
+// console.log(197%2);
+// console.log(301%2);
+
+// function isEven(number){
+//     const remainder= number %2;
+//     // console.log(remainder);
+//     if(remainder===0){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+// const myNumIsEven=isEven(113);
+// const hisNumIsEvenifEven=isEven(180);
+// console.log(myNumIsEven);
+// console.log(hisNumIsEvenifEven);
+
+// function isEven(number){
+//     const remainder = number%2;
+
+//     if(remainder===0){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+
+// }
+// const isEvenOrNot=isEven(110);
+// const isEvenOrOdd=isEven(21);
+// console.log(isEvenOrNot);
+// console.log(isEvenOrOdd);
+
+// function isleapYear (year){
+//     var leapYear = year%4;
+//     if(leapYear==0){
+//     return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+// const myyearisleapyear=isleapYear(2002);
+// console.log( 'my year', myyearisleapyear);
+// const heryearIsLeapYear=isleapYear(2004);
+// console.log('her year',heryearIsLeapYear);
+
+
+
+
+// function isleapYear (year){
+//     var leapYear = year%4;
+//     if(leapYear==0){
+//     return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+// const myyearisleapyear=isleapYear(2002);
+// console.log( 'my year', myyearisleapyear);
+// const heryearIsLeapYear=isleapYear(2004);
+// console.log('his year',heryearIsLeapYear);
+
+
+
+// var array=[5,7,8,10,45,30];
+// function findOddSum(){
+//     var divide=array/2;
+//     if(divide==0){
+
+//     }
+// }
+
+// function getSum(numbers){
+//     let sum=0;
+   
+//     for(let i=0;i<numbers.length;i++){
+//         const index=i;
+//         const element =numbers[index];
+//         sum =sum+element;
+//         console.log(index,element,sum);
+//         //
+//     }
+//     return  sum;
+// }
+// const myNumbers=[12,45,67,24,98,67];
+// getSum(myNumbers);
+
+
+
+// function getSum(numbers){
+//     let sum=0;
+//     for(let i=0;i<numbers.length;i++){
+//         // const index=i;
+//         const element=numbers[i];
+//         sum=sum+element;
+//         // console.log(i,element,sum);
+//     }
+//     return sum;
+// }
+// function getOddNumber(numbers){
+//     let freeSpaceArray=[];
+//     for(var i=0;i<numbers.length;i++){
+//         const index=i;
+//         const element=numbers[index];
+//         if(element%2===1){
+//             console.log(index,element);
+//             freeSpaceArray.push(element);
+//         } 
+//     }
+//     return freeSpaceArray;
+// }
+// const myNumbers=[12,45,23,76,45,65];
+
+// const freeSpaceArray=getOddNumber(myNumbers);
+// console.log(freeSpaceArray);
+// const sum=getSum(freeSpaceArray);
+// console.log(sum);
+
+// // function sumOfNumbers(numbers){
+
+
+// // let sum=0;
+// // for(let i=1; i<=numbers.length ;i++){
+// //     sum=sum+i;
+// //     console.log(i,sum);
+// // }
+// // }
+// // const number=[1,2,3,4,5,6,7,8];
+// // const sums=sumOfNumbers(number);
+// // console.log(sums);
+
+// function multiplicationOfNumbers(number){
+//     let result = 1;
+//     for(let i=1;i<=number;i++){
+//         result = result*i;
+        
+
+//     }
+//     return result;
+// }
+// const result= multiplicationOfNumbers(3);
+// console.log(result);
+
+
+// function factorial(number){
+//     let result=1;
+//     for(let i=number;i>0;i--){
+//         result=result*i;
+//         console.log(i);
+//     }
+//     return result;
+// }
+// const number=5;
+// const fact=factorial(number);
+// console.log(fact);
+
+
+// function getFactorial(number){
+//     let factorial =1;
+
+//     for(let i=1;i<=7;i++){
+//         factorial=factorial*i;
+//         console.log(i,factorial);
+//     }
+//     return factorial;
+// }
+// const factorial=getFactorial(4);
+// console.log(factorial);
+
+
+// function myName(firstName, lastName){
+//     let fullName=`my full name is ${firstName} ${lastName}`;
+   
+// }
+// myName("sadia","sadia");
+// // console.log(fullName);
+
+
+// const string ={
+//     name: 'bangladesh',
+//     districts: 64,
+//     slice:function(){
+
+//     },
+    
+// };
+
+// const s="bnagla";
+// console.log(s.length);
+
+
+
+
+// let text = "Apple, Banana, Kiwi";
+// console.log( text.slice(9,13));
+
+// let text = "Apple, Banana, Kiwi";
+// let part = text.slice(-12);
+
+
+// let str = "Apple, Banana, Kiwi";
+// let part = str.substring(7, 13);
+// 
+// let str = "Apple, Banana, Kiwi";
+// let part = str.substr(7);
+// console.log(part);
+// let str ="i love Sakina & Sakina";
+// let zarina=str.replace("Sakina","zarina");
+// console.log(zarina);
+
+// let text ="hello WORLD";
+// console.log(typeof text.charAt(500));
+
+// console.log(text.split(" "));
+
+
+// function inchToFeet(inch){
+//     const feet =inch/12;
+//     return feet;
+// }
+
+// function inchToFeet2(inch){
+//     const feetFraction =inch/12;
+//     const feetNumber = parseInt(feetFraction);
+//     const inchRemaining = inch % 12;
+//     const result = feetNumber + ' fit'  + " " +inchRemaining + ' inch';
+//     return result;
+    
+// }
+// const shuvoHeight = inchToFeet(75);
+// // console.log(shuvoHeight);
+// const shuvoHeight2 = inchToFeet2(75);
+// // console.log(shuvoHeight2);
+
+
+// function mileToKilometer (mile){
+//     const kilo = mile * 1.60934;
+//     return kilo;
+// } 
+// const mileKilo = mileToKilometer(5);
+// console.log(mileKilo);
+
+// function kilometerToMiles(kilo){
+//     const mile = kilo * 0.621271;
+//     return mile;
+
+// }
+// const KiloMile = kilometerToMilesr(5);
+// console.log(KiloMile);
+
+
+
+// const biryaniKhor = ['abul', 'babul','abul','mabul','tabul','mabul'];
+
+// const numbers = [3,5,5,1,89,45,2,89,5,41,45];
+
+
+// function noDuplicate(array){
+//     const unique =[];
+//     for(const item of array){
+//         if(unique.includes(item) === false){
+//             unique.push(item);
+//         }
+//     }
+//     return unique;
+// }
+// const uniqueArray = noDuplicate(biryaniKhor);
+// console.log(uniqueArray);
+
+// 
+// const Math = {
+//     min: function min(num1,num2){
+//         return num2;
+//     }
+// }
+// // console.log(Math.min);
+
+// const min = Math.min(45,1 ,56,99, -21);
+// console.log(min);
+// const max = Math.max(45,1,67,-99,5,43,111111111111111123);
+// console.log(max);
+// console.log(Math.PI);
+// console.log(Math.abs(5-88));
+
+// console.log(Math.round(2.85));
+// console.log(Math.floor(2.99));
+// console.log(Math.ceil(2.1));
+
+
+// console.log(Math.random()*10);
+
+const rand = Math.round(Math.random()*10);
+// console.log(rand);
+
+
+
+// const today = new Date();
+// const date = new Date('2062-10-09');
+// console.log(date.getDay());
+// const specificDate = new Date(2091, 0, 26);
+// console.log(specificDate);
+// specificDate.setMonth(10);
+// console.log(specificDate.toLocaleString('en-BG'));
+
+
+//   let a =5;
+//   let b= 7;
+//   console.log(a,b);
+// const temp = a;
+// a = b;
+// b = temp;
+//   console.log(a,b);
+
+let x = 9;
+let y = 7;
+//  console.log(x, y);
+ [x,y]=[y,x];
+//  console.log(x,y);
+
+
+// const disa =55;
+// const sal =5000;
+// if(disa>salman){
+//     console.log('disha will get the strawberry');
+// }
+// else{
+//     console.log("salman will get");
+// }
+//inside a function
+
+// function getMax(num1,num2){
+//     if(num1>num2){
+//         return num1;
+//     }
+//     else{
+//         return num2;
+//     }
+// }
+// const max1 = getMax(23,67);
+// const max2 = getMax(33,61);
+// const ultimateMax = getMax(max1,max2);
+// console.log('max o f two is:',ultimateMax);
+
+
+// const tim = 58;
+// const kim = 75;
+// const sim = 81;
+
+// if(tim>kim && tim>sim){
+//     console.log('tim is ultimate max');
+// }
+// else if (kim>tim && kim>sim){
+//     console.log('kim is the max')
+// }
+// else{
+//     console.log('sim is the max');
+// }
+
+
+
+// const max = Math.max(12,6,81,26)
+// console.log(max);
+
+
+// function max (num1, num2, num3){
+//     if(num1>num2 && num1>num3){
+//         return 'tim is ultimate max';
+//     } else if (num2 > num3 && num2 > num1) {
+//         return 'kim is the max';
+//     } else {
+//         return 'sim is the max';
+// }
+// }
+
+// const maxim = max(65,91,1);
+// console.log(maxim);
+
+
+
+// const heights = [23,45,6,1,98,32,11,90,71];
+// function getMax(numbers){
+//     let max = numbers[0];
+//     for(const num of numbers){
+      
+//         if(num>max){
+//             max=num;
+//         }
+//     }
+//     return max;
+   
+// }
+// const max =getMax(heights);
+// console.log(max);
+
+
+
+// const heights = [2,5,4,1,7,9,1,6,4,3,1]; 
+
+// function getMax(numbers){
+//     let max = numbers[0];
+//     for(const num of numbers){
+
+       
+//         if(num>max){
+//             max = num;
+//         }
+//     }
+//    return max;
+// }
+
+// const maxin = getMax(heights);
+// console.log(maxin);
+
+// const height = [9,6,2,1,7,5,8,3,4];
+
+// function minimum(numbers){
+
+//     let min = numbers[0];
+//     for(const num of numbers){
+//         if(num<min){
+//             min=num;
+//         }
+//     }
+//     return min;
+
+// }
+
+// const mini = minimum(height);
+// console.log(mini);
+
+// function woodQuantity(chairQuantity, tableQuantity, bedQuantity){
+//     const perChairWood = 3;
+//     const perTableWood = 10;
+//     const perBedWood = 50;
+
+//     const chairTotalWood = chairQuantity * perChairWood;
+//     const tableTotalWood = tableQuantity * perTableWood;
+//     const bedTotalWood = bedQuantity * perBedWood;
+//      const totalWood = chairTotalWood + tableTotalWood + bedTotalWood;
+
+//      return totalWood;
+// }
+// const newWood = woodQuantity(1,1,4);
+// console.log(newWood);
+
+
+//shirt price 500*5
+//pant price 300*5
+//shoe price 900*5
+
+// function clothQuantity(shirt, pant, shoe){
+
+//     const shirtQuantity =1;
+//     const pantQuantity =1;
+//     const shoeQuantity =1;
+
+
+//     const totalShirt = shirt * shirtQuantity;
+//     const totalPant = pant * pantQuantity;
+//     const totalShoe = shoe * shoeQuantity;
+
+//     const allTogether = totalPant+ totalShirt + totalShoe;
+
+//     return allTogether;
+// }
+
+// const priceOfAllItems = clothQuantity(500,300,900);
+// console.log(priceOfAllItems);
+
+
+
+
+
+// const prices = [2000,100,5400,2400,1000,500,6700,2900];
+// function getMin(numbers){
+
+// let mini= numbers[0];
+//     for(const num of numbers){
+//         if(num<mini){
+//             mini=num;
+//         }
+//     }
+//     return mini;
+// }
+
+// const cheap = getMin(prices);
+// console.log(cheap);
+// 
+
+
+
+
+
+
+// const mobile = [
+//     {name: 'samsung',price: 40000, camera: '12mp', color: 'black'},
+//     {name: 'xiaomi',price: 22000, camera: '12mp', color: 'purple'},
+//     {name: 'Oppo',price: 35000, camera: '12mp', color: 'blue'},
+//     {name: 'Iphone',price: 120000, camera: '12mp', color: 'black'},
+//     {name: 'Vivo',price: 29000, camera: '12mp', color: 'nevi-blue'},
+//     {name: 'One-plus',price: 35000, camera: '12mp', color: 'sky'},
+//     {name: 'Walton',price: 20000, camera: '12mp', color: 'deep-green'},
+// ];
+
+// function getCheapestPhone (phones){
+//     let min = phones[0];
+//    for( const phone of phones){
+// if(phone.price<min.price){
+//     min = phone;
+// }
+//    }
+//    return min;
+// }
+// const cheap = getCheapestPhone(mobile);
+// console.log(cheap);
+
+
+
+
+// const mobile = [
+//     {name: 'samsung',price: 40000, camera: '12mp', color: 'black'},
+//     {name: 'xiaomi',price: 22000, camera: '12mp', color: 'purple'},
+//     {name: 'Oppo',price: 35000, camera: '12mp', color: 'blue'},
+//     {name: 'Iphone',price: 120000, camera: '12mp', color: 'black'},
+//     {name: 'Vivo',price: 29000, camera: '12mp', color: 'nevi-blue'},
+//     {name: 'One-plus',price: 35000, camera: '12mp', color: 'sky'},
+//     {name: 'Walton',price: 20000, camera: '12mp', color: 'deep-green'},
+// ];
+
+// function getExpensivePhone (phones){
+//     let max = phones[0];
+//    for( const phone of phones){
+// if(phone.price>max.price){
+//     max = phone;
+// }
+//    }
+//    return max;
+// }
+// const expensive = getExpensivePhone(mobile);
+// console.log(expensive);
+
+
+
+
+
+// const products = [
+//     {name: 'shampoo', price: 300},
+//     {name: 'comb', price: 80},
+//     {name: 'shirt', price: 1700},
+//     {name: 'pant', price: 1300},
+// ];
+
+// function getShoppingTotal(){
+//     let sum = 0;
+//     for(const product of products){
+//        sum = sum + product.price;
+//     }
+// return sum;
+// }
+// const total = getShoppingTotal(products);
+// console.log(total);
+
+
+
+const products = [
+    { name: 'shampoo', price: 300, quantity: 2},
+    { name: 'comb', price: 80 ,quantity: 3},
+    { name: 'shirt', price: 1700, quantity: 5},
+    { name: 'pant', price: 1300, quantity: 1},
+];
+function cartTotal(products){
+    for (const product of products){
+        console.log(product);
+    }
+}
+
+const shoppingCart = cartTotal(products);
+console.log(shoppingCart);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
