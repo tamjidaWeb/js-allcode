fb3043e484ce1d0910494f05d6ff6df1924195aa
TAMJIDA AKTER <162369026+tamjidaWeb@users.noreply.github.com>
2025-01-29 20:42:32 +0600

    c

--- function.js
+++ function.js
@@ -23,21 +23,197 @@
 // }  
 // console.log(carName);
 // myFunction();
-let car = {
-    Name: "John",
-    model: "Doe",
-    weight: '50',
-    Color: "blue",
-    start: function (){
-        console.log('car has started');
-    },
-    drive: function(){
-        console.log('car is driving');
-    }
-  };
+// let car = {
+//     Name: "John",
+//     model: "Doe",
+//     weight: '50',
+//     Color: "blue",
+//     start: function (){
+//         console.log('car has started');
+//     },
+//     drive: function(){
+//         console.log('car is driving');
+//     }
+//   };
 
-  console.log(car.weight);
+//   console.log(car.weight);
 //   console.log(car[model]);
 
-car.start();
+// car.start();
+
+
+// function myMove(){
+//     const animate = document.querySelector('#animate');
+//     let pos = 0;
+//     const interval = setInterval(frame,5);
+//     function frame(){
+//         if(pos < 350){
+//         pos++;
+//         animate.style.top=pos + 'px';
+//         animate.style.left = pos + 'px';
+//     }
+//     else{
+//         clearInterval(interval);
+//     }
+//     }
+// }
+// myMove();
+
+// function changeText(id)
+// {
+//     console.log(this);
+//     id.innerHTML ="hello"
+//     const change = document.querySelector('#text');
+//     change.innerText ='hello';
+// }
+
+
+// const button = document.querySelector('#button');
+
+// button.onclick= function(){
+//     console.log('hello')
+// }
+
+
+
+
+// function myFunction(test){
+//     console.log(test)
+// }
+
+
+// const button = document.querySelector('#button')
+// const container = document.querySelector('.container')
+// button.addEventListener('click',function(){
+//     myFunction("hellooo button")
+// },true)
+// container.addEventListener('click',function(){
+//     myFunction("hellooo div")
+// },true)
+
+
+
+// const para = document.createElement('p');
+// const node = document.createTextNode('hello world');
+
+
+// para.appendChild(node);
+// const element = document.getElementById('div1');
+// element.appendChild(para);
+
+// const para = document.createElement('p');
+// const node = document.createTextNode("i'm not busy nooo");
+
+// para.appendChild(node);
+
+// const element = document.getElementById('div1');
+// const p2 = document.getElementById('p2');
+// element.replaceChild(para, p2);
+
+// const p2 = document.getElementById('p2');
+// p2.removeChild();
+
+// const div1 = document.getElementById('div1');
+// const p2 = document.getElementById('p2')
+// div1.removeChild(p2)
+
+
+
+// const counter = document.getElementById('counter');
+// const plusBtn = document.getElementById('plusBtn')
+// const minusBtn = document.getElementById('minusBtn')
+// let counting = 0;
+
+// const updateCounting =(value)=>{
+//     counting = counting +value;
+//     counter.innerHTML = counting;
+//     if(counting >=10){
+//        plusBtn.setAttribute('disabled', true)
+      
+//     }
+   
+//     else{
+//        plusBtn.removeAttribute('disabled', false)
+//     }
+
+  
+
+//     if(counting <= 0){
+//         minusBtn.setAttribute('disabled', true);
+//     }
+//     else{
+//         minusBtn.removeAttribute('disabled', false)
+//     }
+
+// }
+
+// plusBtn.addEventListener('click', ()=>{
+//     updateCounting(1);
+
+// })
+// minusBtn.addEventListener('click', ()=>{
+    
+//     updateCounting(-1);
+// })
+
+
+
+const generateRGBcolor = () => {
+    const r = Math.floor(Math.random()*255);
+    const g = Math.floor(Math.random()*255);
+    const b = Math.floor(Math.random()*255);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+const updateColor = ()=>{
+    const color = generateRGBcolor();
+    const colorBox = document.getElementById('colorBox');
+    colorBox.style.backgroundColor = color;
+    const colorCode = document.getElementById('colorCode');
+    colorCode.innerHTML= color;
+}
+const copyColorCode = () =>{
+    const colorCode = document.getElementById('colorCode');
+    const inputTag = document.createElement('input');
+    document.body.appendChild(inputTag);
+    inputTag.value=colorCode.innerText;
+    inputTag.select();
+    document.execCommand('copy');
+    document.body.removeChild(inputTag);
+    alert('copied on clipboard')
+}
+document.getElementById('copyButton').addEventListener('click', ()=>{
+    copyColorCode();
+})
+
+
+function generateNewColorBtn()
+{
+    updateColor();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
